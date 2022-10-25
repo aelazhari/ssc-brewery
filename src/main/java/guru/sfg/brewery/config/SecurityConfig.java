cb94e5935c235405aeacacdd7194d143c2c2e628
El Azhari, Abdelaziz <abdelaziz.elazhari@finastra.com>
2022-10-25 11:27:05 +0200

    TEST JENKINS

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests( authorize -> {
+				authorize.antMatchers("/","/webjars/**","/login","/resources/**","sdsds").permitAll()
+						.antMatchers(HttpMethod.GET,"/api/v1/beer/**").permitAll()
+						.mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+				})
+				.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin().and()
+				.httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password("{noop}admin")
+				.roles("ADMIN")
+				.and()
+				.withUser("user")
+				.password("{noop}user")
+				.roles("USER");
+	}
+
+	/*
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build();
+
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("user")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(admin,user);
+	}
+
+ */
+}
